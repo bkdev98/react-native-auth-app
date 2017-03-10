9ae97fcbf6a09476ec94c6a6a886af8130c42402
bkdev98 <bkdev98@gmail.com>
2017-03-10 20:26:57 +0700

    Play with text input

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCChveCL-FIypCwPHb1mAFsZzCGeXryWME',
+            authDomain: 'authentication-176db.firebaseapp.com',
+            databaseURL: 'https://authentication-176db.firebaseio.com',
+            storageBucket: 'authentication-176db.appspot.com',
+            messagingSenderId: '518009295294'
+        });
+    }
+
     render() {
         return (
             <View>
-                <Text>App</Text>
+                <Header headerText="Authentication" />
+                <LoginForm />
             </View>
         );
     }
